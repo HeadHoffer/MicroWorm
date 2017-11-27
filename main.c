efdb1077bf65ba5a474911276a3d29994fb5af84
Karjalainen Kim TTV15SP <kimkarjalainen@kamk.fi>
2017-11-27 15:39:23 +0200

    Oispa kaljaa

--- main.c
+++ main.c
@@ -40,8 +40,11 @@
 #define SW4 PD3
 #define SW5 PD4
 
+#define GRID_WIDTH 64
+#define GRID_HEIGHT 32
 
-
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
 int main(void)
 
@@ -52,13 +55,20 @@ int main(void)
 	DDRA = 0xE0;
  
 	LCD_Init_With_Flags(FLAG_LANDSCAPE | FLAG_IGNORE_WHITESPACE);
-	
-	int x = 30;
-	int y = 30;
+
+	int num_snake_cells = 5;
+	char snake[GRID_WIDTH * GRID_HEIGHT][2] = {
+		{ 20, 20 },
+		{ 19, 20 },
+		{ 18, 20 },
+		{ 17, 20 },
+		{ 16, 20 },
+	};
+	char food[2];
 
 	bool up,down,left,right = false;
 
-	int coords[2] = {x,y};
+	DrawBox();
 
 	while(true)
 	{
@@ -82,39 +92,79 @@ int main(void)
 			down = true;
 			up = left = right = false;
 		}
+
 		if(up)
 		{
-			Move(&coords[1], 1);
+			Move(snake, &num_snake_cells, 0);
 		}
 		if(down)
 		{
-			Move(&coords[1], -1);
+			Move(snake, &num_snake_cells, 1);
 		}
 		if(left)
 		{
-			Move(&coords[0], 1);
+			Move(snake, &num_snake_cells, 2);
 		}
 		if(right)
 		{
-			Move(&coords[0], -1);
+			Move(snake, &num_snake_cells, 3);
 		}
 		
-		PrintPixel(coords[0],coords[1]);
+		DrawSnake(snake, num_snake_cells);
 		LCD_Display();
 	}
 }
 
-void PrintPixel(int x, int y)
+void Move(char **snake, int *num_snake_cells, char direction)
 {
-	LCD_Clear();
-	LCD_DrawPixel(x,y);
+	char new_pos[2];
+	new_pos[0] = snake[0][0];
+	new_pos[1] = snake[0][1];
+
+	switch(direction) {
+		case 0: // up
+			new_pos[1]++;
+			break;
+		case 1: // down
+			new_pos[1]--;
+			break;
+		case 2: // left
+			new_pos[0]--;
+			break;
+		case 3: // right
+			new_pos[0]++;
+			break;	
+	}
+
+	int i;
+	for(i = 1; i < *num_snake_cells; i++) {
+		snake[i][0] = snake[i - 1][0];
+		snake[i][1] = snake[i - 1][1];
+	}
+
+	snake[0][0] = new_pos[0];
+	snake[0][1] = new_pos[1];
 }
 
-void Move(int *coord, int moveValue)
+void DrawSnake(char **snake, int num_snake_cells)
 {
-	*coord += moveValue;
+	LCD_ClearArea(1,1,126,62);
+
+	int i;
+	for(i = 0; i < num_snake_cells; i++) {
+		int x = snake[i][0];
+		int y = snake[i][1];
+		int screen_x = x * (SCREEN_WIDTH / GRID_WIDTH);
+		int screen_y = y * (SCREEN_HEIGHT / GRID_HEIGHT);
+		LCD_FillRect(screen_x, screen_y, screen_x + (SCREEN_WIDTH / GRID_WIDTH), screen_y + (SCREEN_HEIGHT / GRID_HEIGHT));
+	}
 }
 
-
-
+void DrawBox()
+{
+	LCD_DrawLine(0,0,128,0); //bottom
+	LCD_DrawLine(127,1,127,63); //left
+	LCD_DrawLine(0,1,0,63); //right
+	LCD_DrawLine(0,63,128,63); //top
+}
 
